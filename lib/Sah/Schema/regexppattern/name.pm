a3811d15b2df5ff1571fa42d25149f3713138b75
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-01-03 15:57:58 +0700

    Add coercion rule

--- lib/Sah/Schema/regexppattern/name.pm
+++ lib/Sah/Schema/regexppattern/name.pm
@@ -7,8 +7,9 @@ package Sah::Schema::regexppattern::name;
 
 our $schema = ['str', {
     summary => "Name of pattern, with module prefix but without the 'Regexp::Pattern'",
-    match => qr/\A\w+((::|/|\.)\w+)+\z/,
+    match => qr!\A\w+((::|/|\.)\w+)+\z!,
     'x.completion' => ['regexppattern_name'],
+    'x.perl.coerce_rules' => ['From_str::normalize_perl_modname'],
 }, {}];
 
 1;
